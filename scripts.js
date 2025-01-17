670407bc6a35c3fdc37a020320c474fa4759c7c0
Agustín Chazarreta <agustinchazarreta00@gmail.com>
2025-01-17 00:18:31 -0300

    Implementación de Scroll suave

--- scripts.js
+++ scripts.js
@@ -5,3 +5,14 @@ toggleButton.addEventListener('click', () => {
     document.body.classList.toggle('light-mode');
     toggleButton.innerHTML = document.body.classList.contains('light-mode') ? '<i class="fa-regular fa-sun"></i>' : '<i class="fa-solid fa-moon" style="color: #ffffff;"></i>';
 });
+
+// Scroll suave para enlaces del navbar
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(e.target.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
